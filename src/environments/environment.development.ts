9bf91c52669b909a90c27e2a81a234db24e74674
lauravrg <lvrodriguez16@outlook.com>
2023-10-12 11:39:07 -0500

    update enviroments

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,7 +2,7 @@ export const environment = {
   supabase: {
     url: 'https://miukkvbmolzkmedddnic.supabase.co',
     anonKey:'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pdWtrdmJtb2x6a21lZGRkbmljIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxMjI5NjUsImV4cCI6MjAxMjY5ODk2NX0.C2g4HFid-_8PXKTH_0u82_vhiMKs4PbPrST1xQR_8Ww',
-    urlRedirect: 'http://localhost:4200/nueva-contra',
+    urlRedirect: 'https://fronted-gulupa.vercel.app/nueva-contra',
   },
   backend:{
     url: 'https://api-gulupa.onrender.com/upload'
